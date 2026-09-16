4260078f311ab9bb6189cc5f652c5719d7f9f15d
rita <ritaalam@teelaunch.com>
2026-09-16 17:26:32 +0300

    first commit

---
+++ components/UI/IconButton.js
@@ -0,0 +1,26 @@
+import {Pressable, StyleSheet} from "react-native";
+import {Ionicons} from "@expo/vector-icons";
+
+function IconButton({icon, size, color, onPress}) {
+    return (
+        <Pressable
+            style={({pressed}) => [styles.button, pressed && styles.pressed]}
+            onPress={onPress}
+        >
+            <Ionicons size={size} color={color} name={icon}/>
+        </Pressable>
+    )
+}
+
+export default IconButton;
+
+const styles = StyleSheet.create({
+    button: {
+        padding:8,
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    pressed: {
+        opacity: 0.7,
+    },
+})
